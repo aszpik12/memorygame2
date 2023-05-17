02d52ff7c4b4f673c0403d0e3dc2720ff6a18c37
Asztrik <asztrikpetrovai@gmail.com>
2023-05-17 20:40:27 +0200

    képek randomizálása

--- Jatekter.js
+++ Jatekter.js
@@ -8,13 +8,18 @@ class Jatekter {
     console.log("Jatekter");
     const ARTICLE = $("article");
     for (let i = 0; i < this.#kepekLista.length; i++) {
-      const kartya = new Kartya(ARTICLE, this.#kepekLista[i]);
+      let random = getRandomInt(this.#kepekLista.length)
+      const kartya = new Kartya(ARTICLE, this.#kepekLista[random]);
     }
 
     $(window).on("fordit", (event)=>{
         this.#kivalasztottKepekLista.push(event.detail)
         console.log(this.#kivalasztottKepekLista)
     })
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
 
   }
 }
